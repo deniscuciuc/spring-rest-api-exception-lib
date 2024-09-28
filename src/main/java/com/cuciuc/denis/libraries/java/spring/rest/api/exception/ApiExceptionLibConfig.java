623e53829cdec81f34df7d919de014d34a941665
Denis Cuciuc <personal.dcuciuc@gmail.com>
2024-09-29 00:41:24 +0300

    Add lib spring config

---
+++ src/main/java/com/cuciuc/denis/libraries/java/spring/rest/api/exception/ApiExceptionLibConfig.java
@@ -0,0 +1,8 @@
+package com.cuciuc.denis.libraries.java.spring.rest.api.exception;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cuciuc.denis.libraries.java.spring.rest.api.exception")
+public final class ApiExceptionLibConfig {}
