f2a16fe11ad98c5b6eb30186068737d774909a25
Denis Cuciuc <personal.dcuciuc@gmail.com>
2024-09-29 19:12:58 +0300

    Add java doc to config class

--- src/main/java/com/cuciuc/denis/libraries/java/spring/rest/api/exception/ApiExceptionLibConfig.java
+++ src/main/java/com/cuciuc/denis/libraries/java/spring/rest/api/exception/ApiExceptionLibConfig.java
@@ -3,6 +3,15 @@ package com.cuciuc.denis.libraries.java.spring.rest.api.exception;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for the API exception library.
+ *
+ * <p>This class is used to enable component scanning for the library.
+ *
+ * @see ApiError, ApiErrorResponse, ApiException, ApiExceptionHandler
+ * @author Denis Cuciuc
+ * @since 0.0.1
+ */
 @Configuration
 @ComponentScan("com.cuciuc.denis.libraries.java.spring.rest.api.exception")
 public class ApiExceptionLibConfig {}
